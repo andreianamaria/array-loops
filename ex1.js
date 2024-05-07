0093b74003384c761691893bd02783c352e1a8fb
Andrei Ana-Maria <ana.maria.andrei@ibm.com>
2024-05-07 19:22:35 +0300

    ex1: printeaza numerele pare

---
+++ ex1.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+const printEvenNumbers = function (n) {
+    let sir = `Numerele pare de la 0 la ${n} sunt: `;
+    for (let i = 0; i <= n; i += 2) {
+        sir += i + ', ';
+    }
+    return sir.slice(0, -2);
+}
+n = prompt("Te rog sa introduci un numar: ");
+console.log(printEvenNumbers(n));
