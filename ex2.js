e15c552600f82260034784088a4876513824fd2d
Andrei Ana-Maria <ana.maria.andrei@ibm.com>
2024-05-07 19:29:06 +0300

    printeaza suma

---
+++ ex2.js
@@ -0,0 +1,12 @@
+const printSum = function (arr) {
+    let suma = 0;
+    for (let i = 0; i < arr.length; i ++) {
+        suma += arr[i];
+    }
+    return suma;
+}
+const arr1 = [1, -5, 20, -34, 16, 29, 36, -4];
+const arr2 = [3.45, -2.68, 356, -75.96, 64, 19.28];
+
+console.log(printSum(arr1));
+console.log(printSum(arr2));
