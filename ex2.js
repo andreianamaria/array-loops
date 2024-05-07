b73ba1eabb94d3e48629c5ffa61ba9453ba31782
Andrei Ana-Maria <ana.maria.andrei@ibm.com>
2024-05-07 19:33:33 +0300

    varianta 2 pt suma

--- ex2.js
+++ ex2.js
@@ -5,8 +5,20 @@ const printSum = function (arr) {
     }
     return suma;
 }
+
+const printSumDoi = function (arr) {
+    let suma = 0;
+    arr.map((elem) => {
+        suma += elem;
+    });
+    return suma;
+}
+
 const arr1 = [1, -5, 20, -34, 16, 29, 36, -4];
 const arr2 = [3.45, -2.68, 356, -75.96, 64, 19.28];
 
 console.log(printSum(arr1));
 console.log(printSum(arr2));
+
+console.log(printSumDoi(arr1));
+console.log(printSumDoi(arr2));
